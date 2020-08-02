eaf4798d986b8315c12117ac2483c5be3558635e
Frank Buono <fbuonojr@fordham.edu>
2020-08-01 23:11:28 -0400

    added comments on js file

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// set time at top of page
 var currentDay = moment().format('dddd , MMMM Do');
 console.log(currentDay);
 $("#currentDay").text(currentDay);
+
+//save button to save text to local storage
+$(".save-button").on("click", function(){
+    var hourlyevent = document.getElementById("inputEvent").value;
+});
+
+// color coded timeblocks by past (success), present(warning), or future(primary) 
