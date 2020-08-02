5298660633ed223acdb0d9f8daba905c28149b9c
Frank Buono <fbuonojr@fordham.edu>
2020-08-02 01:18:02 -0400

    save button ids added

--- script.js
+++ script.js
@@ -4,10 +4,29 @@ $("#currentDay").text(currentDay);
 
 //save button to save text to local storage
 $(".save-button").on("click", function(){
-    var hourlyevent = document.getElementById("inputEvent").value;
+    var hourlyEvent = document.getElementById("inputEvent").value;
+    localStorage.setItem("Hourly Event", hourlyEvent);
+    console.log(hourlyEvent);
 });
 
 // color coded timeblocks by past (success), present(warning), or future(primary) 
 // moment.isBefore(), moment.isSame(), moment.isAfter()
 var currentHour = moment().format('h');
+console.log(currentHour)
+var timeIndex = 9;
+var changeColor = function(){
+    for(var i = 0; i < 8; i++){
+        if(timeIndex === 12){
+            timeIndex = 0;
+        }
+        if($("#time-" + timeIndex).attr("class") = "AM"){
+            if($("#time-" + timeIndex).attr("value") = currentHour){
 
+            }
+        }
+        
+        timeIndex++;
+        console.log(timeIndex);
+    }
+}
+console.log($("#time-5").attr("class"));
