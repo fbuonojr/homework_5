932635227a6cd5db3924159b994f12f7d99ea5a4
Frank Buono <fbuonojr@fordham.edu>
2020-08-02 00:49:43 -0400

    starting color coding

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // set time at top of page
 var currentDay = moment().format('dddd , MMMM Do');
-console.log(currentDay);
 $("#currentDay").text(currentDay);
 
 //save button to save text to local storage
@@ -9,3 +8,6 @@ $(".save-button").on("click", function(){
 });
 
 // color coded timeblocks by past (success), present(warning), or future(primary) 
+// moment.isBefore(), moment.isSame(), moment.isAfter()
+var currentHour = moment().format('h');
+
