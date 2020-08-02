2ef465f12fe017396523e90a01618a4470d982c8
Frank Buono <fbuonojr@fordham.edu>
2020-08-02 01:29:17 -0400

    save button ids added

--- script.js
+++ script.js
@@ -2,13 +2,6 @@
 var currentDay = moment().format('dddd , MMMM Do');
 $("#currentDay").text(currentDay);
 
-//save button to save text to local storage
-$(".save-button").on("click", function(){
-    var hourlyEvent = document.getElementById("inputEvent").value;
-    localStorage.setItem("Hourly Event", hourlyEvent);
-    console.log(hourlyEvent);
-});
-
 // color coded timeblocks by past (success), present(warning), or future(primary) 
 // moment.isBefore(), moment.isSame(), moment.isAfter()
 var currentHour = moment().format('h');
@@ -30,3 +23,49 @@ var changeColor = function(){
     }
 }
 console.log($("#time-5").attr("class"));
+
+//save button to save text to local storage
+$(".save-button-9").on("click", function(){
+    var nineEvent = document.getElementById("input-9").value;
+    localStorage.setItem("Nine Event", nineEvent);
+});
+
+$(".save-button-10").on("click", function(){
+    var tenEvent = document.getElementById("input-10").value;
+    localStorage.setItem("Ten Event", tenEvent);
+});
+
+$(".save-button-11").on("click", function(){
+    var elevenEvent = document.getElementById("input-11").value;
+    localStorage.setItem("Eleven Event", elevenEvent);
+});
+
+$(".save-button-12").on("click", function(){
+    var twelveEvent = document.getElementById("input-12").value;
+    localStorage.setItem("Twelve Event", twelveEvent);
+});
+
+$(".save-button-1").on("click", function(){
+    var oneEvent = document.getElementById("input-1").value;
+    localStorage.setItem("One Event", oneEvent);
+});
+
+$(".save-button-2").on("click", function(){
+    var twoEvent = document.getElementById("input-2").value;
+    localStorage.setItem("Two Event", twoEvent);
+});
+
+$(".save-button-3").on("click", function(){
+    var threeEvent = document.getElementById("input-3").value;
+    localStorage.setItem("Three Event", threeEvent);
+});
+
+$(".save-button-4").on("click", function(){
+    var fourEvent = document.getElementById("input-4").value;
+    localStorage.setItem("Four Event", fourEvent);
+});
+
+$(".save-button-5").on("click", function(){
+    var fiveEvent = document.getElementById("input-5").value;
+    localStorage.setItem("Five Event", fiveEvent);
+});
